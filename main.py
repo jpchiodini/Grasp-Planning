5ea92b7a05a0fb15702214947b1aa4d7c6501f85
jpchiodini <chiodini@bu.edu>
2018-03-28 22:47:14 -0400

    #pre-compute all symbolic math to speed up processing. Should be ready for "real-time" now.

--- main.py
+++ main.py
@@ -5,30 +5,34 @@ import pyefd
 import Grasping
 import cv2 as cv
 
-
-#find the contour of the image.
-img = cv.imread('test.png',0)
-edge = cv.Canny(img,100,200)
+# find the contour of the image.
+img = cv.imread('test.png', 0)
+edge = cv.Canny(img, 100, 200)
 _, cnts, _ = cv.findContours(edge.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:1]
+cnts = sorted(cnts, key=cv.contourArea, reverse=True)[:1]
 screenCnt = None
 
 contour_1 = np.vstack(cnts[0]).squeeze()
-plt.plot(contour_1[:,0],contour_1[:,1])
-plt.show()
+# plt.plot(contour_1[:, 0], contour_1[:, 1])
+# plt.show()
 
-#plots clockwise
+# plots clockwise
 # for ii in range(1, len(contour_1)):
 #     plt.plot(contour_1[ii,0], contour_1[ii,1], 'y*', linewidth=2)
 #     plt.show()
 numPts = 200
+order = 4
 
+#pre-calculate symbolic variables so we can solve numerically in the loop.
+px, py, zx, zy, nx, ny = pyefd.initEFDModel(order)
+
+#this part runs in the loop:
+#1) calculate the EFD silhouette:
 locus = pyefd.calculate_dc_coefficients(contour_1)
-coeffs = pyefd.elliptic_fourier_descriptors(contour_1,8)
+coeffs = pyefd.elliptic_fourier_descriptors(contour_1, order)
 
-# xt,yt,dxt,dyt,ddxt,ddyt,crossProd,cbar,sortIdx = pyefd.generateEFDModel(coeffs,locus)
-P,N,Cbar= pyefd.generateEFDModel(coeffs,locus,numPts)
-pyefd.plot_efd(P,N,Cbar,img,contour_1,numPts)
-# Grasping.FindBestGrasps(numPts,P,N,Cbar)
-xLoc,yLoc = Grasping.GraspPointFiltering(numPts,P,N,Cbar)
-pyefd.finalPlot(P,xLoc,yLoc,img,contour_1,numPts)
+#2) Build the grasping point model from silhouette data, and compute best grasp.
+P, N, Cbar = pyefd.generateEFDModel(coeffs, locus, numPts, px, py, zx, zy, nx, ny)
+pyefd.plot_efd(P, N, Cbar, img, contour_1, numPts)
+xLoc, yLoc = Grasping.GraspPointFiltering(numPts, P, N, Cbar)
+pyefd.finalPlot(P, xLoc, yLoc, img, contour_1, numPts)
--- pyefd.py
+++ pyefd.py
@@ -138,7 +138,7 @@ def calculate_dc_coefficients(contour):
     return contour[0, 0] + A0, contour[0, 1] + C0
 
 
-def generateEFDModel(coeffs, locus=(0., 0.), numPts=300):
+def initEFDModel(order):
     a = Symbol('a')
     b = Symbol('b')
     c = Symbol('c')
@@ -146,12 +146,70 @@ def generateEFDModel(coeffs, locus=(0., 0.), numPts=300):
     m = Symbol('m')
     n = Symbol('n')
 
+    a1 = Symbol('a1')
+    a2 = Symbol('a2')
+    a3 = Symbol('a3')
+    a4 = Symbol('a4')
+
+    b1 = Symbol('b1')
+    b2 = Symbol('b2')
+    b3 = Symbol('b3')
+    b4 = Symbol('b4')
+
+    c1 = Symbol('c1')
+    c2 = Symbol('c2')
+    c3 = Symbol('c3')
+    c4 = Symbol('c4')
+
+    d1 = Symbol('d1')
+    d2 = Symbol('d2')
+    d3 = Symbol('d3')
+    d4 = Symbol('d4')
+
+    a_ = [a1, a2, a3, a4]
+    b_ = [b1, b2, b3, b4]
+    c_ = [c1, c2, c3, c4]
+    d_ = [d1, d2, d3, d4]
+
     x = a * cos(2 * n * pi * m) + b * sin(2 * n * pi * m)
     y = c * cos(2 * n * pi * m) + d * sin(2 * n * pi * m)
 
     dx = x.diff(m)
     dy = y.diff(m)
 
+    Zx_sym = 0
+    Zy_sym = 0
+
+    Px = lambdify((a, b, n, m), x)
+    Py = lambdify((c, d, n, m), y)
+    Zx = lambdify((a, b, n, m), dx)
+    Zy = lambdify((c, d, n, m), dy)
+
+    # precomputed symbolic stuff, will be good for real time
+    for n_ in _range(order):
+        dx1 = dx.subs([(a, a_[n_]), (b, b_[n_]), (n, n_ + 1)])
+        dy1 = dy.subs([(c, c_[n_]), (d, d_[n_]), (n, n_ + 1)])
+
+        # symbolic value of dx,dy
+        Zx_sym += dx1
+        Zy_sym += dy1
+
+    Z = sqrt(Zx_sym ** 2 + Zy_sym ** 2)
+    dx_norm = Zx_sym / Z
+    dy_norm = Zy_sym / Z
+    ddx_norm = dx_norm.diff(m)
+    ddy_norm = dy_norm.diff(m)
+
+    tt = [m]
+
+    ax = a_ + b_ + c_ + d_ + tt
+
+    Nx = lambdify(ax, ddx_norm)
+    Ny = lambdify(ax, ddy_norm)
+    return Px, Py, Zx, Zy, Nx, Ny
+
+
+def generateEFDModel(coeffs, locus, numPts, px, py, zx, zy, nx, ny):
     m_ = np.linspace(0, 1.0, numPts)
 
     Px = np.ones((numPts)) * locus[0]
@@ -160,63 +218,37 @@ def generateEFDModel(coeffs, locus=(0., 0.), numPts=300):
     Zx = 0
     Zy = 0
 
-    Zx_sym = 0
-    Zy_sym = 0
+    a = []
+    b = []
+    c = []
+    d = []
 
-    fx1 = lambdify((a, b, n, m), x)
-    fy1 = lambdify((c, d, n, m), y)
-    fdx1_norm = lambdify((a, b, n, m), dx)
-    fdy1_norm = lambdify((c, d, n, m), dy)
-
-    #precompute symbollic stuff, will be good for real time
+    # precompute symbollic stuff, will be good for real time
     for n_ in _range(coeffs.shape[0]):
-        dx1 = dx.subs([(a, coeffs[n_, 0]), (b, coeffs[n_, 1]), (n, n_ + 1)])
-        dy1 = dy.subs([(c, coeffs[n_, 2]), (d, coeffs[n_, 3]), (n, n_ + 1)])
+        a.append(coeffs[n_, 0])
+        b.append(coeffs[n_, 1])
+        c.append(coeffs[n_, 2])
+        d.append(coeffs[n_, 3])
 
-        # symbolic value of dx,dy
-        Zx_sym += dx1
-        Zy_sym += dy1
+        Px += px(a[n_], b[n_], (n_ + 1), m_)
+        Py += py(c[n_], d[n_], (n_ + 1), m_)
+        Zx += zx(a[n_], b[n_], (n_ + 1), m_)
+        Zy += zy(c[n_], d[n_], (n_ + 1), m_)
 
-
-    #this would be the real time portion
-    for n_ in _range(coeffs.shape[0]):
-
-        # numerical values for x,y,dx,dy
-        Px += fx1(coeffs[n_, 0],coeffs[n_, 1],(n_+1),m_)
-        Py += fy1(coeffs[n_, 2],coeffs[n_, 3],(n_+1),m_)
-
-        Zx += fdx1_norm(coeffs[n_, 0],coeffs[n_, 1],(n_+1),m_)
-        Zy += fdy1_norm(coeffs[n_, 2],coeffs[n_, 3],(n_+1),m_)
-
-
-    Z = sqrt(Zx_sym ** 2 + Zy_sym ** 2)
-    dx_norm = Zx_sym / Z
-    dy_norm = Zy_sym / Z
-
-    ddx = Zx_sym.diff(m)
-    ddy = Zy_sym.diff(m)
-    ddx_norm = dx_norm.diff(m)
-    ddy_norm = dy_norm.diff(m)
-
-    fdx1_norm_t = lambdify(m, dx_norm)
-    fdy1_norm_t = lambdify(m, dy_norm)
-    fdx1_norm = lambdify(m, ddx_norm)
-    fdy1_norm = lambdify(m, ddy_norm)
-    fdx1 = lambdify(m, ddx)
-    fdy1 = lambdify(m, ddy)
-
-    Nx_norm = fdx1_norm(m_)
-    Ny_norm = fdy1_norm(m_)
-    Nx = fdx1(m_)
-    Ny = fdy1(m_)
-    Tx = fdx1_norm_t(m_)
-    Ty = fdy1_norm_t(m_)
+    # put together all the variables:
+    N = np.zeros((numPts, 3))
+    for i in range(0, numPts):
+        ax = a + b + c + d
+        ax.append(m_[i])
+        N[i, 0] = nx(*ax)
+        N[i, 1] = ny(*ax)
+        N[i, 2] = 0
 
     # calculate norm of normal vector
-    N = np.zeros((numPts, 3))
-    N[:, 0] = Nx_norm
-    N[:, 1] = Ny_norm
-    N[:, 2] = 0
+    # N = np.zeros((numPts, 3))
+    # N[:, 0] = Nx
+    # N[:, 1] = Ny
+    # N[:, 2] = 0
 
     P = np.zeros((numPts, 3))
     P[:, 0] = Px
@@ -229,11 +261,107 @@ def generateEFDModel(coeffs, locus=(0., 0.), numPts=300):
     crossProd = np.zeros(len(Zx))
     for ii in range(0, len(Zx)):
         aa = np.array([Zx[ii], Zy[ii], 0])
-        bb = np.array([Nx_norm[ii], Ny_norm[ii], 0])
+        bb = np.array(N[ii, :])
         crossProd[ii] = np.cross(aa, bb)[2]
 
     Cbar = np.sign(crossProd) * abs(C)
     return P, N, Cbar
+
+
+# def generateEFDModel(coeffs, locus=(0., 0.), numPts=300):
+#     a = Symbol('a')
+#     b = Symbol('b')
+#     c = Symbol('c')
+#     d = Symbol('d')
+#     m = Symbol('m')
+#     n = Symbol('n')
+#
+#     x = a * cos(2 * n * pi * m) + b * sin(2 * n * pi * m)
+#     y = c * cos(2 * n * pi * m) + d * sin(2 * n * pi * m)
+#
+#     dx = x.diff(m)
+#     dy = y.diff(m)
+#
+#     m_ = np.linspace(0, 1.0, numPts)
+#
+#     Px = np.ones((numPts)) * locus[0]
+#     Py = np.ones((numPts)) * locus[1]
+#
+#     Zx = 0
+#     Zy = 0
+#
+#     Zx_sym = 0
+#     Zy_sym = 0
+#
+#     fx1 = lambdify((a, b, n, m), x)
+#     fy1 = lambdify((c, d, n, m), y)
+#     fdx1_norm = lambdify((a, b, n, m), dx)
+#     fdy1_norm = lambdify((c, d, n, m), dy)
+#
+#     # precompute symbollic stuff, will be good for real time
+#     for n_ in _range(coeffs.shape[0]):
+#         dx1 = dx.subs([(a, coeffs[n_, 0]), (b, coeffs[n_, 1]), (n, n_ + 1)])
+#         dy1 = dy.subs([(c, coeffs[n_, 2]), (d, coeffs[n_, 3]), (n, n_ + 1)])
+#
+#         # symbolic value of dx,dy
+#         Zx_sym += dx1
+#         Zy_sym += dy1
+#
+#     # this would be the real time portion
+#     for n_ in _range(coeffs.shape[0]):
+#         # numerical values for x,y,dx,dy
+#         Px += fx1(coeffs[n_, 0], coeffs[n_, 1], (n_ + 1), m_)
+#         Py += fy1(coeffs[n_, 2], coeffs[n_, 3], (n_ + 1), m_)
+#
+#         Zx += fdx1_norm(coeffs[n_, 0], coeffs[n_, 1], (n_ + 1), m_)
+#         Zy += fdy1_norm(coeffs[n_, 2], coeffs[n_, 3], (n_ + 1), m_)
+#
+#     Z = sqrt(Zx_sym ** 2 + Zy_sym ** 2)
+#     dx_norm = Zx_sym / Z
+#     dy_norm = Zy_sym / Z
+#
+#     ddx = Zx_sym.diff(m)
+#     ddy = Zy_sym.diff(m)
+#     ddx_norm = dx_norm.diff(m)
+#     ddy_norm = dy_norm.diff(m)
+#
+#     fdx1_norm_t = lambdify(m, dx_norm)
+#     fdy1_norm_t = lambdify(m, dy_norm)
+#     fdx1_norm = lambdify(m, ddx_norm)
+#     fdy1_norm = lambdify(m, ddy_norm)
+#     fdx1 = lambdify(m, ddx)
+#     fdy1 = lambdify(m, ddy)
+#
+#     Nx_norm = fdx1_norm(m_)
+#     Ny_norm = fdy1_norm(m_)
+#     Nx = fdx1(m_)
+#     Ny = fdy1(m_)
+#     Tx = fdx1_norm_t(m_)
+#     Ty = fdy1_norm_t(m_)
+#
+#     # calculate norm of normal vector
+#     N = np.zeros((numPts, 3))
+#     N[:, 0] = Nx_norm
+#     N[:, 1] = Ny_norm
+#     N[:, 2] = 0
+#
+#     P = np.zeros((numPts, 3))
+#     P[:, 0] = Px
+#     P[:, 1] = Py
+#     P[:, 2] = 0
+#
+#     C = np.linalg.norm(N, axis=1)
+#
+#     # cross product tells whether we have concave or convex curvature.
+#     crossProd = np.zeros(len(Zx))
+#     for ii in range(0, len(Zx)):
+#         aa = np.array([Zx[ii], Zy[ii], 0])
+#         bb = np.array([Nx_norm[ii], Ny_norm[ii], 0])
+#         crossProd[ii] = np.cross(aa, bb)[2]
+#
+#     Cbar = np.sign(crossProd) * abs(C)
+#     return P, N, Cbar
+
 
 def plot_efd(P, N, Cbar, image=None, contour=None, n=300):
     """Plot a ``[2 x (N / 2)]`` grid of successive truncations of the series.
@@ -263,19 +391,18 @@ def plot_efd(P, N, Cbar, image=None, contour=None, n=300):
     # plt.set_title(str(n + 1))
     # if contour is not None:
     #     plt.plot(contour[:, 0], contour[:, 1], 'c--', linewidth=2)
-    plt.plot(P[:,0], P[:,1], 'r', linewidth=2)
+    plt.plot(P[:, 0], P[:, 1], 'r', linewidth=2)
     if image is not None:
         plt.imshow(image, plt.cm.gray)
     # plt.show()
 
     for ii in range(1, n):
         if Cbar[ii] > 0:
-            plt.plot(P[ii,0], P[ii,1], 'y*', linewidth=2)
+            plt.plot(P[ii, 0], P[ii, 1], 'y*', linewidth=2)
     plt.show()
 
 
-
-def finalPlot(P,finalX,finalY, image=None, contour=None, n=300):
+def finalPlot(P, finalX, finalY, image=None, contour=None, n=300):
     """Plot a ``[2 x (N / 2)]`` grid of successive truncations of the series.
 
     .. note::
@@ -303,10 +430,10 @@ def finalPlot(P,finalX,finalY, image=None, contour=None, n=300):
     # plt.set_title(str(n + 1))
     # if contour is not None:
     #     plt.plot(contour[:, 0], contour[:, 1], 'c--', linewidth=2)
-    plt.plot(P[:,0], P[:,1], 'y', linewidth=2)
+    plt.plot(P[:, 0], P[:, 1], 'y', linewidth=2)
     if image is not None:
         plt.imshow(image, plt.cm.gray)
     # plt.show()
-    plt.plot(P[finalX,0], P[finalX,1], 'r+', linewidth=2)
+    plt.plot(P[finalX, 0], P[finalX, 1], 'r+', linewidth=2)
     plt.plot(P[finalY, 0], P[finalY, 1], 'r+', linewidth=2)
     plt.show()
