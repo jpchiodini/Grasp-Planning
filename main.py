d1b55000f4fd4e98ecf8fe5be82723d0cb926c0c
jpchiodini <jpchiodini@wpi.edu>
2018-04-25 17:52:05 -0400

    change output to color overlay

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ class ListenPublish(object):
         # initialize the model with 4th order efd, and 200 pts
         self.model = Model(4, 200)
         # initialize the publisher
-        self.pub = rospy.Publisher('grasp_coordinates', Float32MultiArray, queue_size=100000000000)
-        self.plot_pub = rospy.Publisher('grasp_plot', Image, queue_size=1000000000)
-        self.sub = rospy.Subscriber('/kinect2/cropped_image/bounding_box', Image, self.callback,queue_size=1, buff_size=2**24)
+        self.pub = rospy.Publisher('grasp_coordinates', Float32MultiArray, queue_size=2)
+        self.plot_pub = rospy.Publisher('grasp_plot', Image, queue_size=2)
+        self.sub = rospy.Subscriber('/kinect2/cropped_image/bounding_box', Image, self.callback, queue_size=1,
+                                    buff_size=2 ** 24)
 
     def run(self):
         self.listener()
@@ -41,8 +42,6 @@ class ListenPublish(object):
         except CvBridgeError as e:
             print(e)
 
-
-
         self.find_current_grasp1(cv_image)
 
         img = self.final_plot(self.model.P, self.point1, self.point2, cv_image, self.model.contour)
@@ -55,20 +54,37 @@ class ListenPublish(object):
              self.model.P[self.point2, 1]]
         pub_array = Float32MultiArray(data=a)
         # ttt = self.bridge.cv2_to_imgmsg(img, "bgr8")
-        ttt = self.bridge.cv2_to_imgmsg(img, "mono8")
+        ttt = self.bridge.cv2_to_imgmsg(img, "bgr8")
         self.pub.publish(pub_array)
         self.plot_pub.publish(ttt)
 
         # print("published locations")
 
     def final_plot(self, P, finalX, finalY, image=None, contour=None):
+
         # plot contours and grasping points in opencv
-        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        # image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        # image = cv.imread('test5.png')
+
         a = P[:, 0].astype(int)
         b = P[:, 1].astype(int)
+        pts = np.zeros((len(a), 2), np.int32)
 
-        idx = np.ravel_multi_index((b, a), (image.shape[0], image.shape[1]))  # extract 1D indexes
-        image.ravel()[idx] = 255
+        # pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+        # cv.imshow("test", image)
+        # cv.waitKey(0)
+
+        # idx = np.ravel_multi_index((b, a), (image.shape[0], image.shape[1]))  # extract 1D indexes
+        # image.ravel()[idx] = 255
+
+        for ii in range(0, len(a)):
+            pts[ii, 0] = a[ii]
+            pts[ii, 1] = b[ii]
+
+        pts = pts.reshape((-1, 1, 2))
+        cv.polylines(image, [pts], True, (255, 255, 255))
+        # cv.imshow("test", image)
+        # cv.waitKey(0)
 
         fx = P[finalX, 0].astype(int)
         fy = P[finalX, 1].astype(int)
@@ -76,12 +92,12 @@ class ListenPublish(object):
         fx1 = P[finalY, 0].astype(int)
         fy1 = P[finalY, 1].astype(int)
 
-        cv.circle(image, (fx, fy), 3, (255, 255, 255), -1)
-        cv.circle(image, (fx1, fy1), 3, (255, 255, 255), -1)
+        cv.circle(image, (fx, fy), 3, (0, 0, 255), -1)
+        cv.circle(image, (fx1, fy1), 3, (0, 0, 255), -1)
 
         # cv.imshow("test", image)
         # cv.waitKey(0)
-        return image # rviz will handle this from now on.
+        return image  # rviz will handle this from now on.
 
     def listener(self):
         rospy.init_node('grasp', anonymous=True)
@@ -128,17 +144,18 @@ class ListenPublish(object):
             if area > 100:
                 contour_list.append(contour)
 
-        cv.drawContours(rawImage, contour_list[0], -1, (255, 0, 0), 2)
+        # cv.drawContours(rawImage, contour_list[0], -1, (255, 0, 0), 2)
         # cv.imshow('Objects Detected', rawImage)
         # cv.waitKey(0)
 
         cnts = sorted(contours, key=cv.contourArea, reverse=True)[:1]
         contour_1 = np.vstack(cnts[0]).squeeze()
 
-        P, N, Cbar = self.model.generate_model(contour_1,rawImage.shape[1],rawImage.shape[0])
+        P, N, Cbar = self.model.generate_model(contour_1, rawImage.shape[1], rawImage.shape[0])
         self.point1, self.point2 = Grasping.GraspPointFiltering(self.model.numPts, P, N, Cbar)
 
     def find_current_grasp(self, img):
+
         # THRESHOLDING BY COLOR
         # rawImage = cv.imread('test5.png')
         # img = cv.cvtColor(rawImage, cv.COLOR_BGR2GRAY)
@@ -147,12 +164,16 @@ class ListenPublish(object):
         # cv.imshow('Original Image', rawImage)
         # cv.waitKey(0)
 
-        #adaptive grey threshold this code may come in usefull.
+        # adaptive grey threshold this code may come in usefull.
         # ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
         # cv.imshow('HSV Image', thresh)
         # cv.waitKey(0)
 
+        blurred = cv.GaussianBlur(rawImage, (11, 11), 0)
+        cv.imshow('blurred Image', blurred)
+        cv.waitKey(0)
         hsv = cv.cvtColor(rawImage, cv.COLOR_BGR2HSV)
+
         # cv.imshow('HSV Image', hsv)
         # cv.waitKey(0)
 
@@ -164,7 +185,6 @@ class ListenPublish(object):
         mask = cv.inRange(hsv, lower_blue, upper_blue)
         # cv.imshow('HSV Image', mask)
         # cv.waitKey(0)
-
 
         # hue, saturation, value = cv.split(hsv)
         # cv.imshow('Saturation Image', hue)
@@ -190,7 +210,6 @@ class ListenPublish(object):
 
         # Floodfill from point (0, 0)
         # cv.floodFill(medianFiltered, mask, (0, 0), 255);
-
 
         # Invert floodfilled image
         # im_floodfill_inv = cv.bitwise_not(medianFiltered)
@@ -221,11 +240,10 @@ class ListenPublish(object):
         cnts = sorted(contours, key=cv.contourArea, reverse=True)[:1]
         contour_1 = np.vstack(cnts[0]).squeeze()
 
-        P, N, Cbar = self.model.generate_model(contour_1,rawImage.shape[1],rawImage.shape[0])
+        P, N, Cbar = self.model.generate_model(contour_1, rawImage.shape[1], rawImage.shape[0])
         self.point1, self.point2 = Grasping.GraspPointFiltering(self.model.numPts, P, N, Cbar)
 
-
-    def find_current_grasp2(self,img):
+    def find_current_grasp2(self, img):
         # My original method
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         img = 255 - img
@@ -247,25 +265,59 @@ class ListenPublish(object):
         edge = cv.Canny(img, 100, 200)
         _, cnts, _ = cv.findContours(edge.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-
-        #cv.drawContours(img, cnts, -1, (255, 0, 0), 2)
-        #cv.imshow('Objects Detected', img)
-        #cv.waitKey(0)
-
+        # cv.drawContours(img, cnts, -1, (255, 0, 0), 2)
+        # cv.imshow('Objects Detected', img)
+        # cv.waitKey(0)
 
         cnts = sorted(cnts, key=cv.contourArea, reverse=True)[:1]
         screenCnt = None
 
         contour_1 = np.vstack(cnts[0]).squeeze()
 
-        P, N, Cbar = self.model.generate_model(contour_1,img.shape[1],img.shape[0])
+        P, N, Cbar = self.model.generate_model(contour_1, img.shape[1], img.shape[0])
         self.point1, self.point2 = Grasping.GraspPointFiltering(self.model.numPts, P, N, Cbar)
         # Grasping.FindBestGrasps(self.model.numPts, P, N, Cbar)
         # PlotUtils.plot_efd(self.model.P, self.model.N, self.model.Cbar, img, contour_1, self.model.numPts)
 
+    def find_current_grasp3(self, img):
 
+        MIN_MATCH_COUNT = 30
 
+        detector = cv.SIFT()
 
+        FLANN_INDEX_KDITREE = 0
+        flannParam = dict(algorithm=FLANN_INDEX_KDITREE, tree=5)
+        flann = cv.FlannBasedMatcher(flannParam, {})
+
+        trainImg = cv.imread("test5.png", 0)
+        trainKP, trainDesc = detector.detectAndCompute(trainImg, None)
+
+        # ret, QueryImgBGR = cam.read()
+        QueryImgBGR = img
+        QueryImg = cv.cvtColor(QueryImgBGR, cv.COLOR_BGR2GRAY)
+        queryKP, queryDesc = detector.detectAndCompute(QueryImg, None)
+        matches = flann.knnMatch(queryDesc, trainDesc, k=2)
+
+        goodMatch = []
+        for m, n in matches:
+            if (m.distance < 0.75 * n.distance):
+                goodMatch.append(m)
+        if (len(goodMatch) > MIN_MATCH_COUNT):
+            tp = []
+            qp = []
+            for m in goodMatch:
+                tp.append(trainKP[m.trainIdx].pt)
+                qp.append(queryKP[m.queryIdx].pt)
+            tp, qp = np.float32((tp, qp))
+            H, status = cv.findHomography(tp, qp, cv.RANSAC, 3.0)
+            h, w = trainImg.shape
+            trainBorder = np.float32([[[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]])
+            queryBorder = cv.perspectiveTransform(trainBorder, H)
+            cv.polylines(QueryImgBGR, [np.int32(queryBorder)], True, (0, 255, 0), 5)
+        else:
+            print
+            "Not Enough match found- %d/%d" % (len(goodMatch), MIN_MATCH_COUNT)
+        cv.imshow('result', QueryImgBGR)
 
 
 if __name__ == '__main__':
